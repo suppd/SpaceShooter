27da5528f93015a0ccd1b31dd787c44bffde3d08
edwin slottje <edwinslottje@gmail.com>
2025-11-27 17:37:54 +0100

    refactor: Add `takeDamage` method to base Entity class

--- src/entities/Entity.js
+++ src/entities/Entity.js
@@ -1,3 +1,4 @@
+// base entity class for all game objects
 export default class Entity {
     constructor(x, y, width, height, color = "white") {
         this.x = x;
@@ -13,5 +14,12 @@ export default class Entity {
     draw(ctx) {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+        takeDamage(amount) {
+        this.hp -= amount;
+        if (this.hp <= 0) {
+            this.dead = true;
+        }
     }
 }
